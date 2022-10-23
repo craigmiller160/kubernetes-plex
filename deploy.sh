dd08aade745b8546e17bb0ec871abfc08482472b
Craig Miller <craigmiller160@gmail.com>
2022-10-23 19:11:14 -0400

    Got deployment to run

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,11 @@ if [ "$exists" == "1" ]; then
   COMMAND=upgrade
 fi
 
+kubectl create ns plex 2>/dev/null
+if [ $? -ne 0 ]; then
+  echo "namespace/plex already exists"
+fi
+
 helm $COMMAND \
   -n $NAMESPACE \
   $NAME \
